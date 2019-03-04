4292bd06f43f2d4ce898bbd533b398c50847c963
LeadNess <43483633+LeadNess@users.noreply.github.com>
2019-03-05 01:42:56 +0300

    Added CREATE request

--- venv/Database.py
+++ venv/Database.py
@@ -9,17 +9,32 @@ class Database(object):
     def close(self):
         self._driver.close()
 
-    def print_greeting(self, message):
+    def print_greeting(self, user):
         with self._driver.session() as session:
-            greeting = session.write_transaction(self.addUser, message)
-            print(greeting)
+            session.write_transaction(self.addUser, user)
 
     @staticmethod
     def addUser(tx, user):
-        name = user['main_info']['first_name'] + ' ' + user['main_info']['last_name']
 
+        user['first_name'] = user['main_info']['first_name']
+        user['last_name']  = user['main_info']['last_name']
+        user['id']         = user['main_info']['id']
 
-        result = tx.run(
-            "CREATE (user:%s) {" % (name,  )
+        tx.run(
+            """
+            CREATE (
+                {id}:User {
+                            first_name: {first_name},
+                            last_name:  {last_name},
+                            main_info:  {main_info},
+                            friends:    {friends},
+                            followers:  {followers},
+                            groups:     {groups},
+                            wall:       {wall},
+                            photos:     {photos}
+                        }                                       
+                )
+            """.format(**user)
+        )
 
 
