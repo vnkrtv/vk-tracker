4684336250afd94c4fa79ac9b1efd21c06fb1920
LeadNess <43483633+LeadNess@users.noreply.github.com>
2019-04-03 01:04:14 +0300

    Created site frame - added some templates

--- VKInfoSite/main/urls.py
+++ VKInfoSite/main/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^user_info/', views.getUserInfo, name='getUserInfo')
+    url(r'^user_info/$', views.getUserDomainShowData, name='getUserDomainShowData'),
+    url(r'^user_changes/$', views.getUserDomainChanges, name='getUserDomainChanges'),
+    url(r'^users_relation/$', views.getUserDomains, name='getUserDomains'),
+    url(r'^user_info/show_data/$', views.getUserInfo, name='getUserInfo'),
+    url(r'^user_changes/show_changes/$', views.getUserChanges, name='getUserChanges'),
+    url(r'^users_relation/show_relation/$', views.getUsersRelations, name='getUsersRelations'),
 ]
--- VKInfoSite/main/views.py
+++ VKInfoSite/main/views.py
@@ -8,8 +8,17 @@ from VK_UserInfo import VK_UserInfo
 def index(request):
     return render(request, 'main/homePage.html')
 
+def getUserDomainShowData(request):
+    return render(request, 'main/getUserDomainDataPage.html')
+
+def getUserDomainChanges(request):
+    return render(request, 'main/getUserDomainChangesPage.html')
+
+def getUserDomains(request):
+    return render(request, 'main/getUsersDomainsPage.html')
+
 def getUserInfo(request):
-    vk_user = VK_UserInfo('b224a255a3de4e95ece62460ff0e8bfa11e67e965daa7eec3b4394c0726540412befb451396083a646007', 'id96354931')
+    vk_user = VK_UserInfo('b224a255a3de4e95ece62460ff0e8bfa11e67e965daa7eec3b4394c0726540412befb451396083a646007', 'n_oriharov')
     user = vk_user.addUserToDatabase()
 
     info = {'value': [
@@ -23,4 +32,10 @@ def getUserInfo(request):
         {'Страна': user['country']},
         {'Город': user['city']}]
     }
-    return render(request, 'main/basic.html', info)
+    return render(request, 'main/userInfo.html', info)
+
+def getUserChanges(request):
+    return render(request, 'main/getUserChanges.html')
+
+def getUsersRelations(request):
+    return render(request, 'main/getUsersRelations.html')
