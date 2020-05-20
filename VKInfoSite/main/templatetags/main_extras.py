3a58a4c946fea1d628b55cc16753f1b6c29a674d
LeadNess <leadness@mail.ru>
2020-05-21 01:11:45 +0300

    Complete user info page
    Update frontend:
    - all tables can be hidden
    - all tables has sorted columns
    Change main info
    Add counters

--- VKInfoSite/main/templatetags/main_extras.py
+++ VKInfoSite/main/templatetags/main_extras.py
@@ -10,6 +10,30 @@ def neo4j_url():
 
 
 @register.simple_tag
+def sex(value):
+    if value == 1:
+        return 'Female'
+    return 'Male'
+
+
+@register.simple_tag
+def languages(value):
+    return ', '.join(value)
+
+
+@register.simple_tag
+def schools(schools_list):
+    s = ''
+    for school in schools_list:
+        if school.get('year_from') and school.get('year_to'):
+            s += '%s %s-%s, ' % (school['name'], school['year_from'], school['year_to'])
+        else:
+            s += '%s, ' % school['name']
+    s = s[:-2]
+    return s
+
+
+@register.simple_tag
 def sort_icon():
     return '/static/main/images/sort.svg'
 
